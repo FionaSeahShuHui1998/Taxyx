b37d86990cea96b03860531f0bd773421c8f5101
FIONA\dunte <fionaseah98@gmail.com>
2025-05-25 01:15:53 +0800

    feat: setup express server

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,15 @@
 console.log("Starting app.js...");
 console.log("app.js successfully started.");
 
-database = require("./database");
+const database = require("./database");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
