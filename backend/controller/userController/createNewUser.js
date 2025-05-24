90ee4ecdccefd4b7d5f351b8083435744683deea
FIONA\dunte <fionaseah98@gmail.com>
2025-05-25 01:51:15 +0800

    feat: create new user

---
+++ backend/controller/userController/createNewUser.js
@@ -0,0 +1,26 @@
+console.log("Creating New User...");
+
+const dbConnection = require("../../database.js");
+
+function creationOfNewUser(
+  username,
+  password,
+  email,
+  firstName,
+  lastName,
+  role
+) {
+  const sqlQuery =
+    "INSERT INTO userHub (username, password, email, firstname, lastname, role) VALUES (?, ?, ?, ?, ?, ?)";
+  const values = [username, password, email, firstName, lastName, role];
+
+  dbConnection.query(sqlQuery, values, (err, result) => {
+    if (err) {
+      console.error("Fail to create new user - ", err.message);
+    } else {
+      console.log("Succussfully created new user.");
+    }
+  });
+}
+
+module.exports = creationOfNewUser;
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,4 +3,20 @@ console.log("Setting up user routes...");
 const express = require("express");
 const router = express.Router();
 
+const creationOfNewUser = require("../controller/userController/createNewUser");
+
+router.post("/createUser", (req, res) => {
+  const { username, password, email, firstName, lastName, role } = req.body;
+
+  if (!username || !password || !email || !firstName || !lastName || !role) {
+    return res
+      .status(400)
+      .json({ error: "Please provide all required fields." });
+  }
+
+  creationOfNewUser(username, password, email, firstName, lastName, role);
+
+  res.status(201).json({ message: "User creation requested." });
+});
+
 module.exports = router;
